f07783550dee2672c27cd268690bbfa6a357c6f7
bhagvat <bhagvat.redde@yash.com>
2022-09-19 10:49:48 +0530

    second commit

--- src/components/serviceModal/ServiceModal.tsx
+++ src/components/serviceModal/ServiceModal.tsx
@@ -33,163 +33,166 @@ export const ServiceModal = () => {
     return true;
   };
   return (
-    <Card className={styles["service-modal"]}>
-      {!isSelectCar ? (
-        <div>
-          <h3>Experience The Best Car Services In Pune</h3>
-          <p>Get instant quotes for your car service</p>
-          <div
-            className={styles["select-car"]}
-            onClick={() => setIsSelectCar(true)}
-          >
-            <span>{yourSelectedCar}</span> <KeyboardArrowDownIcon />
-          </div>
-          <div className={styles["mobile-no"]}>
-            <PhoneInput
-              country="india"
-              placeholder="Enter mobile number"
-              value={value}
-              onChange={setValue}
-            />
-          </div>
-          <Button
-            variant="contained"
-            color="error"
-            style={{ width: "100%", marginBottom: "1em", padding: "0.8em" }}
-          >
-            <span className={styles["check-price-btn"]}>
-              CHECK PRICES FOR FREE
-            </span>
-          </Button>
+    <div className={styles["model-container"]}>
+      <Card className={styles["service-modal"]} sx={{ boxShadow: 10 }}>
+        {!isSelectCar ? (
           <div>
-            By continuing, you agree to GoMechanic's{" "}
-            <a href="#">Terms & Conditions</a>&<a href="#"> Privacy Policy.</a>
-          </div>
-        </div>
-      ) : !model.isCarModel ? (
-        <div>
-          <span>Select Manufacturer</span>
-          <div className={styles["select-car"]}>
-            <input type="search" placeholder="Search Brands" />
-          </div>
-          {loading ? (
-            <PulseLoader
-              className="text-center "
-              color={"#D0021B"}
-              loading={loading}
-              size={20}
-            />
-          ) : (
-            <Grid
-              container
-              spacing={{ xs: 1, md: 2 }}
-              columns={{ xs: 4, sm: 8, md: 12 }}
+            <h3>Experience The Best Car Services In Pune</h3>
+            <p>Get instant quotes for your car service</p>
+            <div
+              className={styles["select-car"]}
+              onClick={() => setIsSelectCar(true)}
             >
-              {CarBrands.map((data, i) => (
-                <Grid item xs={2} sm={4} md={4} key={i}>
-                  <div
-                    className="d-flex flex-column justify-content-center"
-                    onClick={() => {
-                      setSelectedCar({ ...selectedCar, brand: data.name });
-                      setModel({ ...model, isCarModel: true });
-                    }}
-                  >
-                    <div className={styles["car-logo"]}>
-                      <img
-                        src={data.icon}
-                        alt="icon"
-                        style={{ width: "100%", height: "100%" }}
-                      />
-                    </div>
-                    <span>{data.name}</span>
-                  </div>
-                </Grid>
-              ))}
-            </Grid>
-          )}
-        </div>
-      ) : !model.isFuelType ? (
-        <div>
-          <span>Select Model</span>
-          <div className={styles["select-car"]}>
-            <input type="search" placeholder="Search Model" />
-          </div>
-          {loading ? (
-            <PulseLoader
-              className="text-center "
-              color={"#D0021B"}
-              loading={loading}
-              size={20}
-            />
-          ) : (
-            <Grid
-              container
-              spacing={{ xs: 1, md: 2 }}
-              columns={{ xs: 4, sm: 8, md: 12 }}
+              <span>{yourSelectedCar}</span> <KeyboardArrowDownIcon />
+            </div>
+            <div className={styles["mobile-no"]}>
+              <PhoneInput
+                country="india"
+                placeholder="Enter mobile number"
+                value={value}
+                onChange={setValue}
+              />
+            </div>
+            <Button
+              variant="contained"
+              color="error"
+              style={{ width: "100%", marginBottom: "1em", padding: "0.8em" }}
             >
-              {CarModels.map((data, i) => (
-                <Grid item xs={2} sm={4} md={4} key={i}>
-                  <div
-                    className="d-flex flex-column justify-content-center"
-                    onClick={() => {
-                      setSelectedCar({ ...selectedCar, carModel: data.name });
-                      setModel({ ...model, isFuelType: true });
-                    }}
-                  >
-                    <div className={styles["car-logo"]}>
-                      <img
-                        src={data.icon}
-                        alt="icon"
-                        style={{ width: "100%", height: "100%" }}
-                      />
-                    </div>
-                    <span>{data.name}</span>
-                  </div>
-                </Grid>
-              ))}
-            </Grid>
-          )}
-        </div>
-      ) : (
-        <div>
-          <span>Select Fuel</span>
-          <div className={styles["select-car"]}>
-            <input type="search" placeholder="Search Fuel Type" />
+              <span className={styles["check-price-btn"]}>
+                CHECK PRICES FOR FREE
+              </span>
+            </Button>
+            <div>
+              By continuing, you agree to GoMechanic's{" "}
+              <a href="#">Terms & Conditions</a>&
+              <a href="#"> Privacy Policy.</a>
+            </div>
           </div>
-
-          <Grid
-            container
-            spacing={{ xs: 1, md: 2 }}
-            columns={{ xs: 4, sm: 8, md: 12 }}
-          >
-            {FuelType.map((data, i) => (
-              <Grid item xs={2} sm={4} md={4} key={i}>
-                <div
-                  className="d-flex flex-column justify-content-center"
-                  onClick={() => {
-                    setSelectedCar({ ...selectedCar, fuelType: data.name });
-                    setIsSelectCar(false);
-                    setModel({ isCarModel: false, isFuelType: false });
-                    setYourSelectedCar(
-                      `${selectedCar.brand}, ${selectedCar.carModel}, ${data.name}`
-                    );
-                  }}
-                >
-                  <div className={styles["car-logo"]}>
-                    <img
-                      src={data.icon}
-                      alt="icon"
-                      style={{ width: "100%", height: "100%" }}
-                    />
-                  </div>
-                  <span>{data.name}</span>
-                </div>
+        ) : !model.isCarModel ? (
+          <div>
+            <span>Select Manufacturer</span>
+            <div className={styles["select-car"]}>
+              <input type="search" placeholder="Search Brands" />
+            </div>
+            {loading ? (
+              <PulseLoader
+                className="text-center "
+                color={"#D0021B"}
+                loading={loading}
+                size={20}
+              />
+            ) : (
+              <Grid
+                container
+                spacing={{ xs: 1, md: 2 }}
+                columns={{ xs: 4, sm: 8, md: 12 }}
+              >
+                {CarBrands.map((data, i) => (
+                  <Grid item xs={2} sm={4} md={4} key={i}>
+                    <div
+                      className="d-flex flex-column justify-content-center"
+                      onClick={() => {
+                        setSelectedCar({ ...selectedCar, brand: data.name });
+                        setModel({ ...model, isCarModel: true });
+                      }}
+                    >
+                      <div className={styles["car-logo"]}>
+                        <img
+                          src={data.icon}
+                          alt="icon"
+                          style={{ width: "100%", height: "100%" }}
+                        />
+                      </div>
+                      <span>{data.name}</span>
+                    </div>
+                  </Grid>
+                ))}
               </Grid>
-            ))}
-          </Grid>
-        </div>
-      )}
-    </Card>
+            )}
+          </div>
+        ) : !model.isFuelType ? (
+          <div>
+            <span>Select Model</span>
+            <div className={styles["select-car"]}>
+              <input type="search" placeholder="Search Model" />
+            </div>
+            {loading ? (
+              <PulseLoader
+                className="text-center "
+                color={"#D0021B"}
+                loading={loading}
+                size={20}
+              />
+            ) : (
+              <Grid
+                container
+                spacing={{ xs: 1, md: 2 }}
+                columns={{ xs: 4, sm: 8, md: 12 }}
+              >
+                {CarModels.map((data, i) => (
+                  <Grid item xs={2} sm={4} md={4} key={i}>
+                    <div
+                      className="d-flex flex-column justify-content-center"
+                      onClick={() => {
+                        setSelectedCar({ ...selectedCar, carModel: data.name });
+                        setModel({ ...model, isFuelType: true });
+                      }}
+                    >
+                      <div className={styles["car-logo"]}>
+                        <img
+                          src={data.icon}
+                          alt="icon"
+                          style={{ width: "100%", height: "100%" }}
+                        />
+                      </div>
+                      <span>{data.name}</span>
+                    </div>
+                  </Grid>
+                ))}
+              </Grid>
+            )}
+          </div>
+        ) : (
+          <div>
+            <span>Select Fuel</span>
+            <div className={styles["select-car"]}>
+              <input type="search" placeholder="Search Fuel Type" />
+            </div>
+
+            <Grid
+              container
+              spacing={{ xs: 1, md: 2 }}
+              columns={{ xs: 4, sm: 8, md: 12 }}
+            >
+              {FuelType.map((data, i) => (
+                <Grid item xs={2} sm={4} md={4} key={i}>
+                  <div
+                    className="d-flex flex-column justify-content-center"
+                    onClick={() => {
+                      setSelectedCar({ ...selectedCar, fuelType: data.name });
+                      setIsSelectCar(false);
+                      setModel({ isCarModel: false, isFuelType: false });
+                      setYourSelectedCar(
+                        `${selectedCar.brand}, ${selectedCar.carModel}, ${data.name}`
+                      );
+                    }}
+                  >
+                    <div className={styles["car-logo"]}>
+                      <img
+                        src={data.icon}
+                        alt="icon"
+                        style={{ width: "100%", height: "100%" }}
+                      />
+                    </div>
+                    <span>{data.name}</span>
+                  </div>
+                </Grid>
+              ))}
+            </Grid>
+          </div>
+        )}
+      </Card>
+    </div>
     // </Box>
   );
 };
