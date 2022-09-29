e134fe3feebed94dbb352e4d83d0ae93f0119c15
pujashivankar <101718234+pujashivankar@users.noreply.github.com>
2022-09-29 11:58:49 +0530

    logic of see more button

--- src/components/servicePriceComp/reviewsPeriodicServices/Reviews.tsx
+++ src/components/servicePriceComp/reviewsPeriodicServices/Reviews.tsx
@@ -1,13 +1,16 @@
 import React from "react";
-
+import ArrowCircleDownIcon from '@mui/icons-material/ArrowCircleDown';
 import Grid from "@mui/material/Grid";
 import { Dash_saperator } from "../../dash-saperator/Dash_saperator";
 import StarIcon from '@mui/icons-material/Star';
 import styles from "./Reviews.module.css";
 import { reviewData } from "./reviewData";
+import { useState } from "react";
+import ArrowCircleUpIcon from '@mui/icons-material/ArrowCircleUp';
 
 export function Reviews({ data }: any) {
-
+    const [review, setReview] = useState(3);
+    const [btn, setBtn] = useState(false);
 
     return (
         <>
@@ -19,12 +22,12 @@ export function Reviews({ data }: any) {
 
                     <Grid
                         container
-                        spacing={{ xs: 1, md: 3 }}
+                        // spacing={{ xs: 1, md: 3 }}
                         columnSpacing={{ md: 0 }}
-                        rowSpacing={{ md: 3 }}
+                        rowSpacing={{ md: 0 }}
                         columns={{ xs: 4, sm: 4, md: 12 }}
                     >
-                        {data1.content.map((data: any, i: number) => (
+                        {data1.content.slice(0, review).map((data: any, i: number) => (
 
                             <div className={styles["reviews-main"]}>
                                 <div className="row">
@@ -33,20 +36,20 @@ export function Reviews({ data }: any) {
                                             className={styles["review-img"]}
                                             src={data.Image}
                                             alt="img"
-                                            style={{ width: "3em", height:"3em"}}
+                                            style={{ width: "3em", height: "3em" }}
                                         />
                                     </div>
 
                                     <div className="col-md-8">
                                         <span className={styles["review-name"]}>{data.title}</span>
-                                        <div >
-                                            {data.star.map(() => (<StarIcon color="warning" />))}
+                                        <div className="" >
+                                            {data.star.map(() => (<StarIcon color="warning" fontSize="small" />))}
 
                                         </div>
 
                                     </div>
                                     <div className="col-md-3">
-                                        <p className={styles["public-time"]}> Published 3 months ago</p>
+                                        <p className={styles["public-time"]}> {data.time}</p>
                                     </div>
                                 </div>
                                 <div>
@@ -55,10 +58,43 @@ export function Reviews({ data }: any) {
                             </div>
                         ))}{" "}
                     </Grid>
-
-
-
                 </>))}
+                <div className="mb-4 text-center">
+
+                    {!btn && (
+                        <>
+                            <span style={{ marginRight: "1em" }}>SEE MORE</span>
+                            <ArrowCircleDownIcon
+                                fontSize="medium"
+                                onClick={() => {
+                                    setReview(reviewData[0].content.length);
+                                    setBtn(true);
+                                }}
+                                className={styles["see-more"]}
+                            >
+
+                            </ArrowCircleDownIcon>
+                        </>
+                    )}
+                    {btn && (
+                        <>
+                            <span style={{ marginRight: "1em" }}>SEE LESS</span>
+                            <ArrowCircleUpIcon
+                                onClick={() => {
+                                    setReview(3);
+                                    setBtn(false);
+                                }}
+
+                            >
+
+
+                            </ArrowCircleUpIcon >
+                        </>
+                    )}
+
+                </div>
+
+
             </div>
 
             <Dash_saperator />
